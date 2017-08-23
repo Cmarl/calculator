159d2b8c7e7b6d5d97d994d0b16294c672879d0c
Clyde G Marl <marl.clyde@gmail.com>
2017-08-23 13:25:22 -0700

    calcs

--- lib/calculator.ex
+++ lib/calculator.ex
@@ -1,18 +1,47 @@
 defmodule Calculator do
-  @moduledoc """
-  Documentation for Calculator.
-  """
+  def start do
+    spawn(fn -> loop(0) end)
+  end
 
-  @doc """
-  Hello world.
+  defp loop(current_val) do
+    new_val = receive do
+      {:value, client_id} -> send(client_id, {:response, current_val})
+        current_val
+      {:add, value} -> current_val + value
+      {:sub, value} -> current_val - value
+      {:mult, value} -> current_val * value
+      {:div, value} -> current_val / value
 
-  ## Examples
+      invalid_request -> IO.puts("Invalid Request #{{inspect invalid_request}}")
+        current_val
+    end
+    loop(new_val)
+  end
 
-      iex> Calculator.hello
-      :world
+  def value(server_id) do
+    send(server_id, {:value, self()})
+    receive do
+      {:response, value} -> value
+    end
+  end
 
-  """
-  def hello do
-    :world
+  def add(server_id, value) do
+    send(server_id, {:add, value})
+    value(server_id)
+  end
+
+  def sub(server_id, value) do
+    send(server_id, {:sub, value})
+    value(server_id)
+  end
+
+  def mult(server_id, value) do
+    send(server_id, {:mult, value})
+    value(server_id)
+  end
+
+  def div(server_id, value) do
+    send(server_id, {:div, value})
+    value(server_id)
   end
 end
